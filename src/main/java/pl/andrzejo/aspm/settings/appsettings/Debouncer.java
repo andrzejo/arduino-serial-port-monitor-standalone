946876fff323e07e9199936fe7834d6dc5baa63d
Andrzej Oczkowicz <a.oczkowicz@crif.com>
2022-07-04 22:19:50 +0200

    Cleanup

--- src/main/java/pl/andrzejo/aspm/settings/appsettings/Debouncer.java
+++ src/main/java/pl/andrzejo/aspm/settings/appsettings/Debouncer.java
@@ -1,7 +1,5 @@
 package pl.andrzejo.aspm.settings.appsettings;
 
-import org.slf4j.LoggerFactory;
-
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -10,7 +8,7 @@ import java.util.function.Consumer;
 public class Debouncer<T> {
     private static final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
     private final int millis;
-    private Consumer<AppSetting<T>> handler;
+    private final Consumer<AppSetting<T>> handler;
     private long nextExecTimestamp;
     private AppSetting<T> current;
 
@@ -24,7 +22,6 @@ public class Debouncer<T> {
         if (nextExecTimestamp != 0 && nextExecTimestamp < System.currentTimeMillis()) {
             nextExecTimestamp = 0;
             handler.accept(current);
-            LoggerFactory.getLogger("ddd").info("Debouncd {}", current);
         }
     }
 
