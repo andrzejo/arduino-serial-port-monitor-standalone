6e2129b439eb5fe2509f07fc92f913abfdd7bfe6
andrzejo <andrzejoczkowicz@gmail.com>
2022-07-11 23:16:25 +0200

    Update copyright

--- src/main/java/pl/andrzejo/aspm/settings/repository/SettingsRepository.java
+++ src/main/java/pl/andrzejo/aspm/settings/repository/SettingsRepository.java
@@ -1,3 +1,10 @@
+/*
+ * Arduino Serial Port Monitor - Standalone (https://github.com/andrzejo/arduino-serial-port-monitor-standalone)
+ * This is free software (GPL v.2).
+ *
+ * Copyright (c) Andrzej Oczkowicz 2022.
+ */
+
 package pl.andrzejo.aspm.settings.repository;
 
 import pl.andrzejo.aspm.utils.AppFiles;
